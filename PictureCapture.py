cbafb3764554e210459b2a9104f4ab238cfe0cc4
unknown <coco_1998_2@163.com>
2020-08-21 17:06:45 +0800

    make PictureCapture.py a class

--- PictureCapture.py
+++ PictureCapture.py
@@ -1,11 +1,17 @@
+# Copyright (c) All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
 #-*-coding:utf-8-*-
-'''
+
+"""
 @File    : PictureCapture.py
 @Time    : 2020/08/10
 @Author  : Coco Xu
 @Version : 1.0
-@Desc    : Take picture from usb camera and save into local folder, per TimeInveral by using OpenCV.
-'''
+@Desc    : Take jpg image from usb camera and save into local folder, using OpenCV.
+           Image is named by the time it was taken, eg:"2020-08-21-14-03-13.jpg"
+"""
 
 import cv2
 import os
@@ -13,38 +19,67 @@ import datetime
 
 import utility
 
-
-TIME_INTERVAL = 10*60 # 视频帧计数间隔频率
-IMAGE_FOLDER = 'images'
-Count = 1
-
-utility.mkdir(IMAGE_FOLDER)
-     
-vc = cv2.VideoCapture(1)  # 读入视频文件,将视频文件地址写入即可 ('Test.avi')
-# vc.NamedWindow("Face detection", 1)
-
-if vc.isOpened(): # 判断是否正常打开
-    rval , frame = vc.read()
-    print("===========================\r\nCamera Opened Successfully.\r\n===========================\r\n")
-else:
-    rval = False
-    print("===========================\r\nWARNING : No Video Source\r\n===========================\r\n")
+class PictureCapture(object):
+    def __init__(self, imageFolderName, cameraIndex, interval):
+        print("============\rPictureCapture::__init__()")
+        self.imageFolderName = imageFolderName
+        self.cameraIndex = cameraIndex
+        self.interval = interval
+        self.count = 1
     
-timeF = TIME_INTERVAL
-    
-while rval:   # 循环读取视频帧
-    cv2.imshow("usb camera", frame)
-    rval, frame = vc.read()
-    
-    key = cv2.waitKey(20)
-    if key == 27:  # exit on ESC key
-        break
+    def __enter__(self):
+        utility.mkdir(self.imageFolderName)
 
-    if(Count % timeF == 0): # 每隔timeF帧进行存储操作
-        print("\ncount %s\n" % str(Count) )
+        return self
+
+    def __savePicture(self, frame):
         current_time_str = datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d-%H-%M-%S')
-        cv2.imwrite(IMAGE_FOLDER + '/' + current_time_str + '.jpg', frame) # 存储为图像
-        Count = Count + 1
+        cv2.imwrite(self.imageFolderName + '/' + current_time_str + '.jpg', frame)  # 存储为图像
+        print("===========\r\nImage "+ current_time_str +"saved Successfully.\r\n========\r\n")
+
+
+    def takePicture(self):
+        vc = cv2.VideoCapture(self.cameraIndex)  # 若读入视频文件,将视频文件地址写入('Test.avi')
+
+        if vc.isOpened(): 
+            print("===========\r\nCamera Opened Successfully.\r\n========\r\n")
+            rval , frame = vc.read()
+            self.__savePicture(frame)
+        else:
+            rval = False
+            print("===========\r\nWARNING : No Video Source\r\n===========\r\n")
     
-vc.release()
-cv2.destroyWindow("usb camera")
+        vc.release()
+        cv2.destroyWindow("usb camera")
+    
+    
+    def showVideoWindow(self):
+        timeF = self.interval
+     
+        vc = cv2.VideoCapture(self.cameraIndex)  # 若读入视频文件,将视频文件地址写入('Test.avi')
+
+        if vc.isOpened(): 
+            print("===========\r\nCamera Opened Successfully.\r\n========\r\n")
+            rval , frame = vc.read()
+        else:
+            rval = False
+            print("===========\r\nWARNING : No Video Source\r\n===========\r\n")
+
+        while rval:   # 循环读取视频帧
+            cv2.imshow("usb camera", frame)
+            rval, frame = vc.read()
+            
+            key = cv2.waitKey(20)
+            if key == 27:  # exit on ESC key
+                break
+
+            if(self.count % timeF == 0): # 每隔timeF帧进行存储操作
+                print("\ncount %s\n" % str(self.count) )
+                self.__savePicture(frame)
+                self.count = self.count + 1
+    
+        vc.release()
+        cv2.destroyWindow("usb camera")
+        
+    def __exit__(self, exception_type, exception_value, traceback):
+        cv2.destroyAllWindows()    
